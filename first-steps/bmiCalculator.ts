e1081c930517fa41bbd835b2984ac6cf42adc173
David Pérez <deividpa022@gmail.com>
2024-10-14 23:48:36 -0500

    Add .gitignore, bmiCalculator, package-lock.json, package.json, and tsconfig.json files

---
+++ first-steps/bmiCalculator.ts
@@ -0,0 +1,17 @@
+function calculateBmi(height: number, weight: number): string {
+    const heightInMeters = height / 100;
+    const bmi = weight / (heightInMeters * heightInMeters);
+  
+    if (bmi < 18.5) {
+      return 'Underweight';
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+      return 'Normal range';
+    } else if (bmi >= 25 && bmi < 29.9) {
+      return 'Overweight';
+    } else {
+      return 'Obese';
+    }
+  }
+  
+  console.log(calculateBmi(180, 74));
+  
