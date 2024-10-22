89954ce6ba86a57d76972980033a30a958c6bd03
David Pérez <deividpa022@gmail.com>
2024-10-21 23:02:36 -0500

    Refactor patients.ts to include POST route for adding new patients

--- patientor/backend/src/routes/patients.ts
+++ patientor/backend/src/routes/patients.ts
@@ -1,11 +1,24 @@
-import express, { Response } from 'express';
+import express, { Request, Response } from 'express';
 import patientService from '../services/patientService';
 import { NonSensitivePatient } from '../types';
+import toNewPatient from '../utils';
 
 const router = express.Router();
 
 router.get('/', (_req, res: Response<NonSensitivePatient[]>) => {
   res.send(patientService.getNonSensitivePatients());
+});
+
+router.post('/', (req: Request, res: Response) => {
+  try {
+    const newPatient = toNewPatient(req.body);
+    const addedPatient = patientService.addPatient(newPatient);
+    res.json(addedPatient);
+  } catch (e) {
+    if (e instanceof Error) {
+      res.status(400).send(e.message);
+    }
+  }
 });
 
 export default router;
